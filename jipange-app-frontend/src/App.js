2b5837c6bead4cc895ead800258fd20671976447
jimbrian <ui.ux.jimbrian@gmail.com>
2022-11-07 15:08:07 +0300

    imports

--- jipange-app-frontend/src/App.js
+++ jipange-app-frontend/src/App.js
@@ -1,11 +1,10 @@
-import React from 'react'
+import * as React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Homepage from './components/Homepage';
+import NewItemForm from './components/NewItemForm';
+import NewCategoryForm from './components/NewCategoryForm';
+import CategoryList from './components/CategoryList';
+import { dataURL } from './Global';
+import DeleteCategoryForm from './components/DeleteCategoryForm';
 
-function App() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
-
-export default App
