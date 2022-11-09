9ada888a0d0eb4545ea28a101301feda1bf497fa
Anthony Mwaniki <anthonymwangi072@gmail.com>
2022-11-09 18:08:27 +0300

    updated frontend

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import {Card, CardActionArea, CardContent, CardMedia, Typography } from '@mui/material'
+import '../Css/Home.css'
+import img from '../components/Images/job.jpeg'
+
 
 const Homepage = () => {
   return (    
@@ -10,12 +13,12 @@ const Homepage = () => {
           “Don't stress about your events.Welcome to Jipange!!.”
           </Typography>          
         </CardContent>
-        <CardMedia
+        {/* <CardMedia
           component="img"
           height="400"
           image="https://images.unsplash.com/photo-1620608964186-18a8cfc2ad44?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHBsYW5uZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
           alt="planner"
-        />
+        /> */}
       </CardActionArea>
     </Card>    
   )
